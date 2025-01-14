a4dd312db761574d4f65984c8819e9e76096e04a
rafaelthedev <rafael.santos.araujo313@gmail.com>
2025-01-14 23:24:22 +0000

    novo commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe, eu te amo muito, muito, muitooooooooooooooooooooooooooooooooo!!</h1>
+    return <h1>Mãe, eu te amo muito, muito, muitooo"
 }
 
 export default Home;
